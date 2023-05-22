4f77fb48c9555f4b30c9590b738f9a3c21cade4e
jaylordibe <jaylordibe@gmail.com>
2023-05-23 00:16:39 +0800

    Updated docker image

--- push.sh
+++ push.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Push to docker repository
-docker tag docker-react-node-react-node:latest jaylordibe/react-node:18
-docker push jaylordibe/react-node:18
+docker tag docker-react-node-react-node:latest jaylordibe/react-node:16
+docker push jaylordibe/react-node:16
 
 # Push to github repository
 git add .
